44f04a354ff33a1b1325e5f27539a56790cd5eff
Md.Solaiman <md.solaiman366882@gmail.com>
2023-08-20 21:32:50 +0600

    JS Feature added and some card added

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+function getInputValue(inputId){
+    return document.getElementById(inputId).value;
+}
 function getValue(Id){
     return parseFloat(document.getElementById(Id).innerText);
 }
@@ -5,26 +8,68 @@ function setInnerText(id,text){
     document.getElementById(id).innerText = text;
 }
 
+let discountPrice = 0;
+
+ function getDiscount(newPrice){
+        const discountInput = getInputValue('discount-input');
+        if(discountInput === 'SELL200'){
+            const discountPrice = (newPrice * 20) / 100;
+            return discountPrice;
+        }
+        else{
+            const discountInitial = 0;
+            return discountInitial;
+        }
+     
+ }
+
+
 function productEntry(productName,price){
     const productEntry = document.getElementById('product-entry');
     const previousTotalPrice = getValue('total-price');
-    const total = document.getElementById('total')
+    const totalDisplay = document.getElementById('total');
     const h4 = document.createElement('h4');
     const count = productEntry.childElementCount  +  1;
     const newPrice = (previousTotalPrice + price).toFixed(2);
+    const totalDisplayPrice = newPrice - discountPrice;
+
     if(newPrice >0){
         document.getElementById('purchase-btn').removeAttribute('disabled');
+    }else{
+        document.getElementById('purchase-btn').setAttribute('disabled');
     }
+
     if(newPrice >= 200){
         document.getElementById('coupon-btn').removeAttribute('disabled');
         
     }
+
     h4.innerHTML = `
         <span>${count}</span>. <span>${productName}<span>
     `;
     h4.classList.add('text-2xl','font-medium' ,'text-heading-color');
     productEntry.appendChild(h4);
     setInnerText('total-price',newPrice);
-    setInnerText('total',newPrice);
-    console.log(newPrice);
+    setInnerText('total',totalDisplayPrice);
 }
+
+document.getElementById('coupon-btn').addEventListener('click',function(){
+    const totalPrice = getValue('total-price');
+    const discountInput = getInputValue('discount-input');
+    if(discountInput === 'SELL200'){
+        discountPrice = (totalPrice * 20) / 100;
+        const totalDisplay = totalPrice - discountPrice;
+        setInnerText('discount-amount',discountPrice.toFixed(2));
+        setInnerText('total',totalDisplay.toFixed(2));
+    }else{
+        return alert('You Have Entered Wrong Coupon Code');
+    }
+    
+});
+
+document.getElementById('home-btn').addEventListener('click',function(){
+    setInnerText('total-price','00.00');
+    setInnerText('discount-amount','00.00');
+    setInnerText('total','00.00');
+    document.getElementById('product-entry').innerHTML = '';
+});
